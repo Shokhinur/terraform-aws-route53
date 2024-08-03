0b7ebb9682cd6442775fa678b33bf2596519aba6
Shokhinur <shokhinur992@gmail.com>
2024-08-03 18:02:32 -0500

    creation

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "name"{
+  type = string
+  default= ""
+  description = "Please replace defualt var"
+}
+
+variable "type" {
+    type =string
+    default = ""
+    description = "Please replace with your"
+}
+
+variable "env" {
+    type = string
+    default = ""
+    description = "subdomain env name"
+}
+
